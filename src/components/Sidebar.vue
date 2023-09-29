cc89ccab22300e56c80303957292356dbd41c837
Thenuri <cb010529@students.apiit.lk>
2023-09-29 11:48:21 +0530

    sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -22,7 +22,7 @@
                                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                             </svg>
                         </span>
-                        <span class="ml-2 text-sm tracking-wide truncate">Settings</span>
+                       
                     </router-link>
                 </li>
                 <li>
